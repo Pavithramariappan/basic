1f2d4ce9a40d58befba0e813b0936271265983d0
Pavithramariappan <paveeithra@gmail.com>
2019-08-10 19:48:21 +0530

    Create large_among_n.py

---
+++ large_among_n.py
@@ -0,0 +1,9 @@
+a=input("enter a value")
+b=input("enter b value")
+c=input("enter c value")
+if(a>b)and(a>c):
+    print("a is greater")
+if(b>a)and(b>c):
+    print("b is greater")
+if(c>a)and(c>b):
+    print("c is greater")
